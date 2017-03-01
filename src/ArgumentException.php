64adc57c77384a4ab05e73abb6e8ef9fc5445520
thinkingmedia <mathew@thinkingmedia.ca>
2017-03-01 07:09:02 -0500

    updated

--- src/ArgumentException.php
+++ src/ArgumentException.php
@@ -15,21 +15,4 @@ class ArgumentException extends cgTagException
     {
         parent::__construct($message, $code, $previous);
     }
-
-
-    /**
-     * @return array
-     */
-    public function getCallerTrace(): array
-    {
-        foreach ($this->getTrace() as $trace) {
-            $func = $trace['function'];
-            $class = $trace['class'];
-            $args = $trace['args'];
-
-            if (Strings::startsWith($class, 'cgTag\Exceptions')) {
-                continue;
-            }
-        }
-    }
 }
